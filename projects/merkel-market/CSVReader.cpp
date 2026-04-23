bd1cabcfb64f774be724df098b83cb8148728842
smile14 <smile.pakawat@gmail.com>
2026-04-23 11:33:30 +0700

    Implement StringToOBE mehtod

--- projects/merkel-market/CSVReader.cpp
+++ projects/merkel-market/CSVReader.cpp
@@ -1,18 +1,35 @@
 #include "CSVReader.h"
+#include <iostream>
 
 CSVReader::CSVReader() {}
 
-std::vector<OrderBookEntry> readCsv(std::string csvFile) {
+std::vector<OrderBookEntry> CSVReader::readCsv(std::string csvFile) {
   std::vector<OrderBookEntry> entries;
   return entries;
 }
 
-std::vector<std::string> tokenise(std::string csvLine, char separator) {
+std::vector<std::string> CSVReader::tokenise(std::string csvLine,
+                                             char separator) {
   std::vector<std::string> tokens;
   return tokens;
 }
 
-OrderBookEntry stringsToOBE(std::vector<std::string> strings) {
-  OrderBookEntry obe{1, 1, "", "", OrderBookType::bid};
+OrderBookEntry CSVReader::stringsToOBE(std::vector<std::string> tokens) {
+  double price, amount;
+
+  if (tokens.size() != 5) {
+    std::cout << "Bad line " << std::endl;
+    throw std::exception{};
+  }
+
+  try {
+    price = std::stod(tokens[3]);
+    amount = std::stod(tokens[4]);
+  } catch (std::exception &e) {
+    throw;
+  }
+
+  OrderBookEntry obe{price, amount, tokens[0], tokens[1], OrderBookEntry::stringToOrderBookType(tokens[2])};
+
   return obe;
 }
