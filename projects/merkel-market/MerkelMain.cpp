e8b72a09eb661a00951dc511e274648a23dd0774
smile14 <smile.pakawat@gmail.com>
2026-04-28 16:51:52 +0700

    Change static time to currentTime

--- projects/merkel-market/MerkelMain.cpp
+++ projects/merkel-market/MerkelMain.cpp
@@ -42,8 +42,8 @@ void MerkelMain::printHelp() {
 void MerkelMain::printMarketStats() {
   for (std::string const &p : orderBook.getKnownProducts()) {
     std::cout << "Product: " << p << std::endl;
-    std::vector<OrderBookEntry> entries = orderBook.getOrders(
-        OrderBookType::ask, p, "2020/03/17 17:01:24.884492");
+    std::vector<OrderBookEntry> entries =
+        orderBook.getOrders(OrderBookType::ask, p, currentTime);
     std::cout << "Asks seen: " << entries.size() << std::endl;
     std::cout << "Max ask: " << OrderBook::getHighPrice(entries) << std::endl;
     std::cout << "Min ask: " << OrderBook::getLowPrice(entries) << std::endl;
